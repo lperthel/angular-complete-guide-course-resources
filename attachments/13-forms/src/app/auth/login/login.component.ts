9e385f00740dfd8c5c673267ac7266226cef156f
Leland Perthel <lperthel@gmail.com>
2024-12-08 08:42:10 -0500

    - created a gitignore for .vscode files. - updated rolled back changes to template based forms for debugging

--- attachments/13-forms/src/app/auth/login/login.component.ts
+++ attachments/13-forms/src/app/auth/login/login.component.ts
@@ -1,9 +1,67 @@
-import { Component } from "@angular/core";
+import {
+  afterNextRender,
+  Component,
+  DestroyRef,
+  inject,
+  input,
+  viewChild,
+} from "@angular/core";
+import { FormsModule, NgForm } from "@angular/forms";
+import { debounceTime } from "rxjs";
 
 @Component({
   selector: "app-login",
   standalone: true,
+  imports: [FormsModule],
   templateUrl: "./login.component.html",
   styleUrl: "./login.component.css",
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private form = viewChild.required<NgForm>("form");
+  private destroyRef = inject(DestroyRef);
+  private sessionStorage1 = sessionStorage;
+
+  constructor() {
+    const savedForm = window.localStorage.getItem("saved-login-form");
+
+    if (savedForm) {
+      const loadedFormData = JSON.parse(savedForm);
+      const savedEmail = loadedFormData.email;
+      setTimeout(() => {
+        this.form().controls["email"].setValue(savedEmail);
+      }, 1);
+    }
+    afterNextRender(() => {
+      const sub = this.form()
+        .valueChanges?.pipe(debounceTime(500))
+        .subscribe({
+          next: (val) => {
+            window.localStorage.setItem(
+              "saved-login-form",
+              JSON.stringify({ email: val.email })
+            );
+          },
+        });
+      //unsub logic
+      this.destroyRef.onDestroy(() => sub?.unsubscribe());
+    });
+  }
+
+  onSubmit(formData: NgForm) {
+    if (formData.form.invalid) {
+      return;
+    }
+    console.log("submitting form");
+    console.log("ngForm: ", formData);
+    console.log("viewChild: ", this.form);
+
+    formData.control;
+
+    const email = formData.form.value.email;
+    console.log(email);
+    const password = formData.form.value.password;
+    console.log(password);
+
+    formData.form.reset();
+  }
+}
